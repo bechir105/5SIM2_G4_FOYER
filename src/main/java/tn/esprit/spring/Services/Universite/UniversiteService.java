f863ffc729f70c3af93cddd5af5d8b3e950e64e3
bechir105 <81562063+bechir105@users.noreply.github.com>
2024-11-08 17:47:42 +0100

    Update UniversiteService Log4j
    question 4

--- src/main/java/tn/esprit/spring/Services/Universite/UniversiteService.java
+++ src/main/java/tn/esprit/spring/Services/Universite/UniversiteService.java
@@ -1,41 +1,42 @@
-package tn.esprit.spring.Services.Universite;
-
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import tn.esprit.spring.DAO.Entities.Foyer;
 import tn.esprit.spring.DAO.Entities.Universite;
-import tn.esprit.spring.DAO.Repositories.FoyerRepository;
 import tn.esprit.spring.DAO.Repositories.UniversiteRepository;
-
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UniversiteService implements IUniversiteService {
     UniversiteRepository repo;
 
     @Override
     public Universite addOrUpdate(Universite u) {
+        log.info("Adding or updating Universite: {}", u);
         return repo.save(u);
     }
 
     @Override
     public List<Universite> findAll() {
+        log.info("Fetching all Universites");
         return repo.findAll();
     }
 
     @Override
     public Universite findById(long id) {
-        return repo.findById(id).get();
+        log.info("Finding Universite with ID: {}", id);
+        return repo.findById(id).orElse(null);
     }
 
     @Override
     public void deleteById(long id) {
+        log.warn("Deleting Universite with ID: {}", id);
         repo.deleteById(id);
     }
 
     @Override
     public void delete(Universite u) {
+        log.warn("Deleting Universite: {}", u);
         repo.delete(u);
     }
 }
