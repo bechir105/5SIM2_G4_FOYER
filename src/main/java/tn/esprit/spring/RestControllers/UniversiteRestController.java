27925e5893741c53b061915371709d215abf9c66
bechir105 <81562063+bechir105@users.noreply.github.com>
2024-11-08 17:49:32 +0100

    Update UniversiteRestController Log4j
    question 4

--- src/main/java/tn/esprit/spring/RestControllers/UniversiteRestController.java
+++ src/main/java/tn/esprit/spring/RestControllers/UniversiteRestController.java
@@ -1,44 +1,46 @@
 package tn.esprit.spring.RestControllers;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import tn.esprit.spring.DAO.Entities.Foyer;
 import tn.esprit.spring.DAO.Entities.Universite;
-import tn.esprit.spring.Services.Foyer.IFoyerService;
 import tn.esprit.spring.Services.Universite.IUniversiteService;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("universite")
 @AllArgsConstructor
+@Slf4j
 public class UniversiteRestController {
     IUniversiteService service;
 
     @PostMapping("addOrUpdate")
-    Universite addOrUpdate(@RequestBody Universite u) {
+    public Universite addOrUpdate(@RequestBody Universite u) {
+        log.info("Received request to add or update Universite: {}", u);
         return service.addOrUpdate(u);
     }
 
     @GetMapping("findAll")
-    List<Universite> findAll() {
+    public List<Universite> findAll() {
+        log.info("Received request to find all Universites");
         return service.findAll();
     }
 
     @GetMapping("findById")
-    Universite findById(@RequestParam long id) {
+    public Universite findById(@RequestParam long id) {
+        log.info("Received request to find Universite by ID: {}", id);
         return service.findById(id);
     }
 
     @DeleteMapping("delete")
-    void delete(@RequestBody Universite u) {
+    public void delete(@RequestBody Universite u) {
+        log.warn("Received request to delete Universite: {}", u);
         service.delete(u);
     }
 
     @DeleteMapping("deleteById")
-    void deleteById(@RequestParam long id) {
+    public void deleteById(@RequestParam long id) {
+        log.warn("Received request to delete Universite by ID: {}", id);
         service.deleteById(id);
     }
-
-
 }
