1341fcd7c6ebb3f8b33c7959b890a1f4fd7ca641
medaziz.elarbi <mohamedaziz.elarbi@esprit.tn>
2024-11-14 03:18:20 +0100

    log4j BlocRestController

--- src/main/java/tn/esprit/spring/RestControllers/BlocRestController.java
+++ src/main/java/tn/esprit/spring/RestControllers/BlocRestController.java
@@ -5,46 +5,57 @@ import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.DAO.Entities.Bloc;
 import tn.esprit.spring.Services.Bloc.IBlocService;
 
+import lombok.extern.slf4j.Slf4j;
+
+
 import java.util.List;
 
 @RestController
 @RequestMapping("bloc")
 @AllArgsConstructor
+@Slf4j
 public class BlocRestController {
     IBlocService service;
 
     @PostMapping("addOrUpdate")
-    Bloc addOrUpdate(@RequestBody Bloc b) {
+    public Bloc addOrUpdate(@RequestBody Bloc b) {
+        log.info("Received request to add or update bloc: {}", b);
         return service.addOrUpdate(b);
     }
 
     @GetMapping("findAll")
-    List<Bloc> findAll() {
+    public List<Bloc> findAll() {
+        log.info("Received request to find all Blocs");
         return service.findAll();
     }
 
     @GetMapping("findById")
-    Bloc findById(@RequestParam long id) {
+    public Bloc findById(@RequestParam long id) {
+        log.info("Received request to find Bloc by ID: {}", id);
         return service.findById(id);
     }
 
     @DeleteMapping("delete")
-    void delete(@RequestBody Bloc b) {
+    public void delete(@RequestBody Bloc b) {
+        log.warn("Received request to delete Bloc: {}", b);
         service.delete(b);
     }
 
     @DeleteMapping("deleteById")
-    void deleteById(@RequestParam long id) {
+    public void deleteById(@RequestParam long id) {
+        log.warn("Received request to delete Bloc by ID: {}", id);
         service.deleteById(id);
     }
 
     @PutMapping("affecterChambresABloc")
-    Bloc affecterChambresABloc(@RequestBody List<Long> numChambre, @RequestParam String nomBloc) {
+    public Bloc affecterChambresABloc(@RequestBody List<Long> numChambre, @RequestParam String nomBloc) {
+        log.info("Received request to assign Chambres to Bloc with name: {}", nomBloc);
         return service.affecterChambresABloc(numChambre, nomBloc);
     }
 
     @PutMapping("affecterBlocAFoyer")
-    Bloc affecterBlocAFoyer(@RequestParam String nomBloc,@RequestParam String nomFoyer){
+    public Bloc affecterBlocAFoyer(@RequestParam String nomBloc,@RequestParam String nomFoyer){
+        log.info("Received request to assign Bloc: {} to Foyer: {}", nomBloc, nomFoyer);
         return service.affecterBlocAFoyer(nomBloc,nomFoyer);
     }
 }
